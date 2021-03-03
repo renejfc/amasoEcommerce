23312aeff5bbacc44337a527392268c6d4e25e0a
Eni <estefanielunardi@users.noreply.github.com>
2021-03-03 16:41:20 +0100

    Manifiest: view style

--- resources/views/manifiest.blade.php
+++ resources/views/manifiest.blade.php
@@ -1,44 +1,51 @@
 <x-app-layout>
     <div>
         <h1 class="title text-center pb-10 pt-5 lg:pt-20">Sobre Amasó</h1>
-        <div class=" text-center heroText">
-            <p>Tienda online de productos locales y artesanales para generar un 
-            impacto positivo en la sociedad y el planeta</p>
+        <div class=" text-center pb-10 pt-5 lg:pt-20 text-xl heroText">
+            <p>Tienda online de productos locales y artesanales para generar un
+                impacto positivo en la sociedad y el planeta</p>
         </div>
-        <section>
-            <div>
-                <div class="rounded-xl rounded-b-none object-fill w-full">
-                    <img src="https://images.unsplash.com/photo-1518593929011-2b5ef6be57c7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1014&q=80" alt="Placeholder">
+        <section class="ml-32 mr-32">
+            <div class="flex justify-center items-center overflow-hidden md:-mx-px">
+                <div class="w-full overflow-hidden md:my-px md:px-px xl:w-1/2 rounded-xl">
+                    <img  src="https://images.unsplash.com/photo-1518593929011-2b5ef6be57c7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1014&q=80" alt="Placeholder">
                 </div>
-                <div>
-                    <p>La relacion con nuestros proveedores esta basada en la vertiente humana por enccima
+                <div class="w-full overflow-hidden md:my-px md:px-px xl:w-1/2 boxHeroTextManifiest md:text-lg rounded-xl">
+                    <p class="pb-10 pt-5 mx-16 lg:pt-20 heroTextManifiest w-52 md:w-auto max-w-lg">La relacion con nuestros proveedores esta basada en la vertiente humana por enccima
                     de la comercial.</p>
                 </div>
-                <div>
-                    <p>Ponemos el foco en el origen de los alimentos, siempre respetando 
+            </div>
+            <div class="flex justify-center items-center overflow-hidden md:-mx-px">
+                <div class="w-full overflow-hidden md:my-px md:px-px xl:w-1/2 boxHeroTextManifiest rounded-xl">
+                    <p class="pb-10 pt-5 mx-16 lg:pt-20 heroTextManifiest w-52 md:w-auto max-w-lg">Ponemos el foco en el origen de los alimentos, siempre respetando
                     su temporalidad.</p>
                 </div>
-                <div class="rounded-xl rounded-b-none object-fill w-full">
-                    <img src="https://images.unsplash.com/photo-1600615789274-a87bc22a02fc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGNvbWlkYXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Placeholder">
+                <div class="w-full overflow-hidden md:my-px md:px-px xl:w-1/2 rounded-xl">
+                    <img src="https://images.unsplash.com/photo-1600615789368-e54db9aced0d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fGNvbWlkYXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Placeholder">
                 </div>
-                <div class="rounded-xl rounded-b-none object-fill w-full">
+            </div>
+            <div class="flex justify-center items-center overflow-hidden md:-mx-px">
+                <div class="w-full overflow-hidden md:my-px md:px-px xl:w-1/2 rounded-xl">
                     <img src="https://images.unsplash.com/photo-1456439663599-95b042d50252?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="Placeholder">
                 </div>
-                <div>
-                    <p>Creamos valor económico, medioambiental y social; contribuimos al bienestar
+                <div class="w-full overflow-hidden md:my-px md:px-px xl:w-1/2 boxHeroTextManifiest rounded-xl">
+                    <p class="pb-10 pt-5 mx-16 lg:pt-20 heroTextManifiest w-52 md:w-auto max-w-lg">Creamos valor económico, medioambiental y social; contribuimos al bienestar
                     y al progreso de las generaciones presentes y futuras.</p>
                 </div>
             </div>
         </section>
+
         <section>
-             <h3 class="title">Los valores de nuestros artesanos</h3>
-             <div>
-                <p>Todos los productos artesanales, de nuestra tienda online, son elaborados con
-                materiales de gran calidad, mucha dedicación y amor, para entrgar al consumidor final un producto de gran valor.</p>
-                <br>
-                <p>Creemos en el comercio local y justo, por eso nos comprometemos a ofrecerte una variedad de
-                productos de cercanía.</p>
-             </div>
+            <div class="pt-12 mr-10 ml-10">
+                <h3 class="title text-center pb-10 pt-5 lg:pt-20">Los valores de nuestros artesanos</h3>
+                <div class="text-center text-3xl heroText rounded-xl pb-10 pt-5 lg:pt-20 beigeLightBg">
+                    <p>Todos los productos artesanales, de nuestra tienda online, son elaborados con
+                        materiales de gran calidad, mucha dedicación y amor, para entrgar al consumidor final un producto de gran valor.</p>
+                    <br>
+                    <p>Creemos en el comercio local y justo, por eso nos comprometemos a ofrecerte una variedad de
+                        productos de cercanía.</p>
+                </div>
+            </div>
         </section>
     </div>
 </x-app-layout>
